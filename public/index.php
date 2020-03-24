7a0a87bcc96a8b9c8e70c03012eddcf61658f8cf
MartyRizey <martyrizey@gmail.com>
2020-03-24 15:12:37 +0100

    modifs

--- public/index.php
+++ public/index.php
@@ -2,11 +2,11 @@
 <?php 
   
   // constante magique __DIR__
-  // print_r(__DIR__);
+  // print_r(__DIR__); echo '<p></p>';
 
   // Variable Super Globale $_SERVER
   // echo '<pre>'; 
-  // print_r($_SERVER);
+  // print_r($_SERVER['DOCUMENT_ROOT']);
   // echo'</pre>';
 
   // Avec le fichier 'index.php' à la racine du projet "liens-localhost".
@@ -14,8 +14,11 @@
   // require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';  
   
   // Avec le fichier 'index.php' dans un dossier "public".
+  // $_SERVER['DOCUMENT_ROOT'] = D:/Logiciels/laragon/www
   require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';
+  // __DIR__ = D:\Logiciels\laragon\www\tutoriels\Support_cours\Autres\liens-localhost\public
   require __DIR__ . '\..\views\home.php';
+  
 ?>
    
     
