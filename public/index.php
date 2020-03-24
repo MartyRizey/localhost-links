4f74be9719348a4d691baab5a2a4d18b932d7ab5
MartyRizey <martyrizey@gmail.com>
2020-03-24 08:05:00 +0100

    valider les liens sous Linux dans un environnement Apache/php classic

--- public/index.php
+++ public/index.php
@@ -5,17 +5,18 @@
   // print_r(__DIR__);
 
   // Variable Super Globale $_SERVER
-  // echo '<pre>'; 
+  echo '<pre>'; 
   // print_r($_SERVER);
-  // echo'</pre>';
+  echo'</pre>';
 
   // Avec le fichier 'index.php' à la racine du projet "liens-localhost".
   // require __DIR__ . '/views/home.php';
   // require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';  
   
-  // Avec le fichier 'index.php' dans un dossier "public".
-  require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';
-  require __DIR__ . '\..\views\home.php';
+  // Avec le fichier 'index.php' dans un dossier "public" sous Linux.
+  // sous Linux seul le slash ' / ' est fonctionnel.
+  require $_SERVER['DOCUMENT_ROOT'] . '/Linux/code/localhost-links/views/home.php';
+  require __DIR__ . '/../views/home.php';
 ?>
    
     
--- views/templates/navbar.php
+++ views/templates/navbar.php
@@ -3,7 +3,8 @@
 <nav>
 
  <ul>
-   <li><a href="#">Accueil</a></li>
+   <!-- mon lien est par rapport au fichier index.php -->
+   <li><a href="../views/home.php">Accueil</a></li>
    <li><a href="#">Membres</a></li>
    <li><a href="../views/contact.php">Contact</a></li>
  </ul>
