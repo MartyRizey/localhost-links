c5029b25625599a09a93d18544ea2a260c8c08b8
MartyRizey <martyrizey@gmail.com>
2020-03-24 19:22:28 +0100

    Modifs fichiers index.php - fileDefinePath.php -

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 
 <?php 
   
+  require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/inc/fileDefinePath.php';
+
   // constante magique __DIR__
   // print_r(__DIR__); echo '<p></p>';
 
@@ -13,11 +15,17 @@
   // require __DIR__ . '/views/home.php';
   // require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';  
   
-  // Avec le fichier 'index.php' dans un dossier "public".
+  // Avec le fichier 'index.php' dans un dossier "public". sous Windows via Laragon
+  // ------------------------------------------------------------------------------
   // $_SERVER['DOCUMENT_ROOT'] = D:/Logiciels/laragon/www
   require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';
   // __DIR__ = D:\Logiciels\laragon\www\tutoriels\Support_cours\Autres\liens-localhost\public
   require __DIR__ . '\..\views\home.php';
+  // à partir d'une constante créée dans le fichier inc/fileDefinePath.php
+  require VIEWSFOLDER . '/home.php';
+  // Chemin relatif à partir de l'endroit ou je me trouve
+  require '..\views\home.php';
+  
   
 ?>
    
