bd775df2c126e1316905da65b764c586b1372c64
MartyRizey <martyrizey@gmail.com>
2020-03-27 15:31:04 +0100

    modifs fichier index.php

--- public/index.php
+++ public/index.php
@@ -11,11 +11,12 @@
   // print_r($_SERVER['DOCUMENT_ROOT']);
   // echo'</pre>';
 
-  // Avec le fichier 'index.php' à la racine du projet "liens-localhost".
+  // Avec le fichier 'index.php' à la racine du projet "liens-localhost" sous Windows via laragon
+  // --------------------------------------------------------------------------------------------
   // require __DIR__ . '/views/home.php';
   // require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';  
   
-  // Avec le fichier 'index.php' dans un dossier "public". sous Windows via Laragon
+  // Avec le fichier 'index.php' dans un dossier "public" sous Windows via Laragon
   // ------------------------------------------------------------------------------
   // $_SERVER['DOCUMENT_ROOT'] = D:/Logiciels/laragon/www
   require $_SERVER['DOCUMENT_ROOT'] . '/tutoriels/Support_cours/Autres/liens-localhost/views/home.php';
@@ -25,6 +26,12 @@
   require VIEWSFOLDER . '/home.php';
   // Chemin relatif à partir de l'endroit ou je me trouve
   require '..\views\home.php';
+
+  // Avec le fichier 'index.php' dans un dossier "public" sous Linux.
+  // ----------------------------------------------------------------
+  // sous Linux seul le slash ' / ' est fonctionnel.
+  // require $_SERVER['DOCUMENT_ROOT'] . '/Linux/code/localhost-links/views/home.php';
+  // require __DIR__ . '/../views/home.php';
   
   
 ?>
